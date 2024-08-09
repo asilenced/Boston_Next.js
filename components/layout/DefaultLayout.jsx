178d5f7f5026f421f85e575a2a623d5b5bfa6b03
asilenced <heidegisler335v553@hotmail.com>
2024-08-09 05:37:24 -0700

    comback

--- components/layout/DefaultLayout.jsx
+++ components/layout/DefaultLayout.jsx
@@ -1,10 +1,9 @@
 import Footer from '../shared/Footer';
 import PagesMetaHead from '../PagesMetaHead';
-import loading from '../../public/images/loading.svg';
 
 const DefaultLayout = ({ children }) => {
 	return (
-		<div className='dark:bg-primary-dark' style={{padding: 0, margin: 0, fontFamily: 'Helvetica, Arial, sans-serif'}}>
+		<div style={{background: "black", padding: 0, margin: 0, fontFamily: 'Helvetica, Arial, sans-serif'}}>
 			<PagesMetaHead />
 				<div>
 					{children}
