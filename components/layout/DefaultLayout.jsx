793b51ab02e9bc2c9141373d3fe1c7953832b850
asilenced <heidegisler335v553@hotmail.com>
2024-08-09 18:47:37 -0700

    footerhidden

--- components/layout/DefaultLayout.jsx
+++ components/layout/DefaultLayout.jsx
@@ -1,14 +1,25 @@
+import { useState, useEffect } from 'react';
 import Footer from '../shared/Footer';
 import PagesMetaHead from '../PagesMetaHead';
+import period from '../../data/const';
 
 const DefaultLayout = ({ children }) => {
+	const [isLoading, setIsLoading] = useState(true);
+	
+	useEffect(() => {
+		const timer = setTimeout(() => {
+		  setIsLoading(false);
+		}, period);
+		return () => clearTimeout(timer);
+	}, []);
+	
 	return (
 		<div className='dark:bg-primary-dark' style={{padding: 0, margin: 0, fontFamily: 'Helvetica, Arial, sans-serif'}}>
 			<PagesMetaHead />
 				<div>
 					{children}
 				</div>
-			<Footer />
+			{ !isLoading ? <Footer /> : '' }
 		</div>
 	);
 };
